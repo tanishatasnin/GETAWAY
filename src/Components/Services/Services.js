24ef46e6591e5de8ae31f0cd428d14ab7c45a482
tanishatasin <tasnintanisha@gmail.com>
2021-10-30 20:14:12 +0600

    add more

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -6,7 +6,7 @@ const Services = () => {
                const [services ,setServices] =useState([])
 // _________________ data fetch _____________ 
                useEffect(()=>{
-                              fetch('./data.json')  
+                              fetch('http://localhost:5000/places')  
                               .then(res=>res.json())   
                               .then(data=>setServices(data))  
                       },[])
--- src/Pages/AddPlaces/AddPlaces.js
+++ src/Pages/AddPlaces/AddPlaces.js
@@ -1,9 +1,33 @@
 import React from 'react';
-
+import axios from 'axios';
+import { useForm } from "react-hook-form";
 const AddPlaces = () => {
+               const { register, handleSubmit,reset } = useForm();
+               const onSubmit = data =>{
+                 
+                              console.log(data);
+                              axios.post('http://localhost:5000/places',data)
+                              .then(res=>{
+                                             if(res.data.insertedId){
+                                                            alert('add successfully');
+                                                            reset();
+                                             }
+                              })
+}
                return (
                               <div>
-                                   <h1>ADD a New place</h1>          
+                                   <h1>ADD a New place</h1>
+                                   <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("name", { required: true, maxLength: 20 })} placeholder='Name' />
+      <br />
+      <input {...register("img")} placeholder='png' />
+      <br />
+      <input {...register("description")} placeholder='Description' />
+      <br />
+      <input type="number" {...register("price")} placeholder='price' />
+      <br />
+      <input type="submit" />
+    </form>                
                               </div>
                );
 };
